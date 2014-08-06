36985ec07daf198f20d515e5916b97ddc8ffe940
Sajad <bahmani.s@gmail.com>
2014-08-07 01:28:19 +0430

    Update JalCal.java

--- src/main/java/ir/sjb/jalcal/util/JalCal.java
+++ src/main/java/ir/sjb/jalcal/util/JalCal.java
@@ -17,6 +17,7 @@ package ir.sjb.jalcal.util;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.StringTokenizer;
 import java.util.TimeZone;
 
 /**
@@ -50,6 +51,7 @@ public class JalCal {
         int monthMiladi = c.get(SimplePersianCalendar.MONTH);
         int dayMiladi = c.get(SimplePersianCalendar.DAY_OF_MONTH);
         Calendar cal = Calendar.getInstance();
+
         cal.set(Calendar.DAY_OF_MONTH, dayMiladi);
         cal.set(Calendar.MONTH, monthMiladi);
         cal.set(Calendar.YEAR, yearMiladi);
@@ -62,21 +64,30 @@ public class JalCal {
     }
 
     /**
-     *
      * @param date
-     * @return day/month/year
+     * @param dayAtFirst if true on return value day/month/year else year/month/day
+     * @return day/month/year or year/month/day
      */
-    public static String gregorianToJalaliDate(Date date) {
+    public static String gregorianToJalaliDate(Date date, boolean dayAtFirst) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         SimplePersianCalendar spc = new SimplePersianCalendar();
         spc.setTimeZone(TimeZone.getTimeZone("UTC"));
         spc.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-        return String.valueOf(
-                String.valueOf(spc.getDateFields().getDay()) + "/"
-                + String.valueOf(spc.getDateFields().getMonth() + 1) + "/"
-                + spc.getDateFields().getYear()
-        );
+        if (dayAtFirst) {
+            return String.valueOf(
+                    String.valueOf(spc.getDateFields().getDay()) + "/"
+                    + String.valueOf(spc.getDateFields().getMonth() + 1) + "/"
+                    + spc.getDateFields().getYear()
+            );
+        } else {
+            return String.valueOf(
+                    spc.getDateFields().getYear() + "/"
+                    + String.valueOf(spc.getDateFields().getMonth() + 1) + "/"
+                    + String.valueOf(spc.getDateFields().getDay())
+            );
+        }
+
     }
 
     /**
@@ -92,7 +103,42 @@ public class JalCal {
                 + String.valueOf(calendar.get(Calendar.SECOND));
     }
 
-    public static String gregorianToJalali(Date date) {
-        return gregorianToJalaliDate(date) + "   " + gregorianToJalaliTime(date);
+    /**
+     *
+     * @param date need to be converted
+     * @param dayAtFirst if true on return value day/month/year else year/month/day
+     * @return
+     */
+    public static String gregorianToJalali(Date date, boolean dayAtFirst) {
+        return gregorianToJalaliDate(date, dayAtFirst) + "   " + gregorianToJalaliTime(date);
+    }
+
+    private static int[] slashDateTokenizer(String input) throws DateException {
+        //21/2/1389
+        StringTokenizer tokenizer = new StringTokenizer(input, "/");
+        if (tokenizer.countTokens() != 3) {
+            throw new DateException();
+        }
+        int[] retInt = new int[3];
+        try {
+            retInt[0] = Integer.valueOf(tokenizer.nextToken());
+            retInt[1] = Integer.valueOf(tokenizer.nextToken());
+            retInt[2] = Integer.valueOf(tokenizer.nextToken());
+        } catch (NumberFormatException ex) {
+            throw new DateException();
+        }
+        return retInt;
+    }
+
+    public static Date jalaliToGregorian(String input) throws DateException {
+        int[] intArr = slashDateTokenizer(input);
+        try {
+            Date retDate = jalaliToGregorian(intArr[0], intArr[1], intArr[2], 0, 0, 0);
+            return retDate;
+        } catch (DateException ex) {
+            Date retDate = jalaliToGregorian(intArr[2], intArr[1], intArr[0], 0, 0, 0);
+            return retDate;
+        }
+
     }
 }
